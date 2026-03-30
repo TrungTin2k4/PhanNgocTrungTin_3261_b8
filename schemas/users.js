2d3929d8a0724ddec50d644c7a1e535dd6f90589
Phan Ngọc Trung Tín <phanngoctrungtin79@gmail.com>
2026-03-30 09:10:36 +0700

    hoàn thành

--- schemas/users.js
+++ schemas/users.js
@@ -64,9 +64,13 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-userSchema.pre('save', function (next) {
+userSchema.pre('save', function () {
+  if (!this.isModified('password')) {
+    return;
+  }
+
   let genSalt = bcrypt.genSaltSync(10);
-  let newPass = bcrypt.hashSync(this.password, genSalt)
+  let newPass = bcrypt.hashSync(this.password, genSalt);
   this.password = newPass;
 })
 
